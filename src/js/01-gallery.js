789e9f887b55076818b11023dfe0695ef6098d98
Anatolii <anatoly.tomnyak@gmail.com>
2023-07-31 20:37:15 +0300

    1 task

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,36 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 
+const gallery = document.querySelector('.gallery')
+
+function createGallery(array) {
+    return array
+        .map(({ original, preview, description }) =>
+            `<li class="gallery__item">
+                <a class="gallery__link" href="${original}">
+                    <img
+                        class="gallery__image"
+                        src="${preview}"
+                        data-source="${original}"
+                        alt="${description}"
+                    />
+                </a>
+            </li>`)
+        .join("")
+}
 console.log(galleryItems);
+const result = createGallery(galleryItems)
+
+gallery.insertAdjacentHTML('beforeend', result)
+
+let lightbox = new SimpleLightbox('.gallery a',
+    {
+        captionsData: 'alt',
+        captionDelay: 250,
+    });
+lightbox.on('show.simplelightbox', function () {
+    // do something…
+});
